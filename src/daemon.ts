37c4eb73b92ef7e7ee56ed677fa5b5fcdcf53245
Ty Busby <tybusby@gmail.com>
2026-02-16 11:44:42 -0500

    await super.onstart and check for tty

--- src/daemon.ts
+++ src/daemon.ts
@@ -8,35 +8,38 @@ export class Daemon extends Lifecycle {
 
         this.runnable = runnable;
 
-        process.on("SIGTERM", async () => await this.stop());
-        process.on("SIGINT", async () => await this.stop());
-        process.stdin.setRawMode(true);
-        process.stdin.resume();
-        process.stdin.setEncoding("utf8");
-        process.stdin.on("data", async (key) => {
-            if (key === "\u0003") {
-                // Ctrl+C to exit
-                await this.stop();
-            }
-            if (key.toString().toLowerCase() === "q") {
-                // q to exit
-                await this.stop();
-            }
-            if (key.toString().toLowerCase() === "c") {
-                // Press 'c' to clear
-                process.stdout.write("\x1Bc");
-            }
+        process.on("SIGTERM", () => void this.stop());
+        process.on("SIGINT", () => void this.stop());
 
-            if (key === "\r") {
-                // Enter key → 5 blank lines
-                process.stdout.write("_".repeat(80));
-                process.stdout.write("\n".repeat(5));
-            }
-        });
+        if (process.stdin.isTTY) {
+            process.stdin.setRawMode(true);
+            process.stdin.resume();
+            process.stdin.setEncoding("utf8");
+            process.stdin.on("data", async (key) => {
+                if (key === "\u0003") {
+                    // Ctrl+C to exit
+                    await this.stop();
+                }
+                if (key.toString().toLowerCase() === "q") {
+                    // q to exit
+                    await this.stop();
+                }
+                if (key.toString().toLowerCase() === "c") {
+                    // Press 'c' to clear
+                    process.stdout.write("\x1Bc");
+                }
+
+                if (key === "\r") {
+                    // Enter key → 5 blank lines
+                    process.stdout.write("_".repeat(80));
+                    process.stdout.write("\n".repeat(5));
+                }
+            });
+        }
     }
 
     protected override async onstart(): Promise<void> {
-        super.onstart();
+        await super.onstart();
 
         await Promise.all(this.runnable.map((r) => r.start()));
     }
