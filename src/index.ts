db1e02efbcfc69701c6154815fd0ae32920ec386
Ty Busby <tybusby@gmail.com>
2026-02-17 12:08:34 -0500

    checkpoint

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,7 @@ import "./logging";
 import { Daemon } from "./daemon";
 import { LiveImage } from "./agents/live";
 
+// import { PublisherSession } from "./ws/publisher";
+// new Daemon(new PublisherSession(new LiveImage())).start();
+
 new Daemon(new LiveImage()).start();
