307543c41e34a167f57a46f24ee0b080cdeef9f9
Ty Busby <tybusby@gmail.com>
2026-02-17 13:50:51 -0500

    remove convert

--- src/agents/live.ts
+++ src/agents/live.ts
@@ -3,18 +3,14 @@ import { LogError } from "./errors/log";
 import { Publish } from "./tasks/publish";
 import { C121 } from "./sources/c121";
 import { Watermark } from "./tasks/watermark";
-import { Convert } from "./tasks/convert";
 
 export class LiveImage extends Agent {
     private readonly camera = new C121();
 
     constructor() {
         super(new C121());
-        
+
         this.addImageTask(new Watermark());
-        this.addImageTask(
-            new Convert({ type: "image/jpeg", options: { quality: 70 } }),
-        );
         this.addImageTask(new Publish(this.camera.getID()));
 
         this.addErrorTask(new LogError());
