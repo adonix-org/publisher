f07a3debdb4b915741e62791111cfa79d4431dee
Ty Busby <tybusby@gmail.com>
2026-02-17 12:00:34 -0500

    add to string

--- src/agents/errors/log.ts
+++ src/agents/errors/log.ts
@@ -4,4 +4,8 @@ export class LogError implements ErrorTask {
     public async handle(err: ImageError): Promise<void> {
         console.error(err.source, err.error.message);
     }
+
+    public toString(): string {
+        return "LogError";
+    }
 }
