1f67a3025564f76a9f337fd16c0164a996ab195d
Ty Busby <tybusby@gmail.com>
2026-02-17 09:23:30 -0500

    added publisher

---
+++ src/tasks/publisher.ts
@@ -0,0 +1,35 @@
+import { ImageBuffer, ImageTask } from "./interfaces";
+
+const POST_URL_BASE = process.env.LIVEIMAGE_BASE;
+const ADMIN_KEY = process.env.LIVEIMAGE_ADMIN_KEY;
+
+export class Publisher implements ImageTask {
+    constructor(private readonly sourceId: string) {}
+
+    public async process(
+        image: ImageBuffer,
+        signal: AbortSignal,
+    ): Promise<ImageBuffer | null> {
+        const response = await fetch(`${POST_URL_BASE}/${this.sourceId}`, {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${ADMIN_KEY}`,
+                "Content-Type": image.contentType,
+            },
+            body: new Uint8Array(image.buffer),
+            signal,
+        });
+
+        console.debug(this.toString(), await response.text());
+
+        if (!response.ok) {
+            throw new Error(await response.text());
+        }
+
+        return image;
+    }
+
+    public toString(): string {
+        return "[Publisher]";
+    }
+}
--- src/tasks/rtsp-agent.ts
+++ src/tasks/rtsp-agent.ts
@@ -2,11 +2,14 @@ import { Source } from "../interfaces";
 import { TaskAgent } from "./tasks";
 import { RtspSource } from "./rtsp";
 import { LogError } from "./errors";
+import { Publisher } from "./publisher";
 
 export class RtspAgent extends TaskAgent {
     constructor(source: Source) {
         super(new RtspSource(source));
-        
+
+        this.addImageTask(new Publisher(source.id));
+
         this.addErrorTask(new LogError());
     }
 
