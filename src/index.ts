0b64e961d37a5b6a9e53244029aa4beb204169e6
Ty Busby <tybusby@gmail.com>
2026-02-18 12:08:17 -0500

    enable ws

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import "./logging";
 
 import { Daemon } from "./daemon";
 import { LiveImage } from "./agents/live";
-// import { PublisherSession } from "./ws/publisher";
+import { PublisherSession } from "./ws/publisher";
 
-// new Daemon(new PublisherSession(new LiveImage())).start();
+new Daemon(new PublisherSession(new LiveImage())).start();
 
-new Daemon(new LiveImage()).start();
+// new Daemon(new LiveImage()).start();
