b258b85c2b3ad0c03e07af43274beed092e656a4
Ty Busby <tybusby@gmail.com>
2026-02-18 09:56:45 -0500

    color overlay

---
+++ src/agents/tasks/image/color.ts
@@ -0,0 +1,38 @@
+import sharp from "sharp";
+import { ImageBuffer, ImageTask } from "../../interfaces";
+
+export class ColorOverlay implements ImageTask {
+    constructor(private readonly color: string | sharp.Color = "black") {}
+
+    public async process(image: ImageBuffer): Promise<ImageBuffer | null> {
+        const meta = await sharp(image.buffer).metadata();
+        const width = meta.width;
+        const height = meta.height;
+
+        const colorOverlay = await sharp({
+            create: {
+                width,
+                height,
+                channels: 4,
+                background: this.color,
+            },
+        })
+            .png()
+            .toBuffer();
+
+        const buffer = await sharp(image.buffer)
+            .composite([
+                {
+                    input: colorOverlay,
+                    blend: "over",
+                },
+            ])
+            .toBuffer();
+
+        return { buffer, contentType: image.contentType };
+    }
+
+    public toString(): string {
+        return `[ColorOverlay-${JSON.stringify(this.color)}]`;
+    }
+}
