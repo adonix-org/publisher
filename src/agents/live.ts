2c7965a06a79fb02aa775e279c29a3990eab5f78
Ty Busby <tybusby@gmail.com>
2026-02-19 12:29:41 -0500

    rename folder

--- src/agents/live.ts
+++ src/agents/live.ts
@@ -3,7 +3,7 @@ import { LogError } from "../tasks/error/log";
 import { C121 } from "../sources/c121";
 import { Watermark } from "../tasks/transform/watermark";
 import { MaxErrors } from "../tasks/error/max";
-import { Profiler } from "../tasks/profile/profiler";
+import { Profiler } from "../tasks/observe/profiler";
 import { Publish } from "../tasks/transfer/publish";
 
 export class LiveImage extends Agent {
