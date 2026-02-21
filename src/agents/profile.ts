5c3ef9599ef4d29506fafdaed2f3edee59f1fc61
Ty Busby <tybusby@gmail.com>
2026-02-21 05:50:08 -0500

    base constructor params

--- src/agents/profile.ts
+++ src/agents/profile.ts
@@ -1,11 +1,12 @@
 import { AgentSource } from ".";
+import { Lifecycle } from "../lifecycle";
 import { ImageTask } from "../tasks";
 import { Profiler } from "../tasks/observe/profiler";
 import { Agent } from "./agent";
 
 export class ProfileAgent extends Agent {
-    constructor(source: AgentSource) {
-        super(source);
+    constructor(source: AgentSource, ...children: Lifecycle[]) {
+        super(source, ...children);
     }
 
     public override addImageTask(task: ImageTask): void {
