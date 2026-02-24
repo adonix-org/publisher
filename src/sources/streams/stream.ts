2d46f503dc63a0d70c258dff6ffea1619719a742
Ty Busby <tybusby@gmail.com>
2026-02-24 10:33:36 -0500

    cleanup

--- src/sources/streams/stream.ts
+++ src/sources/streams/stream.ts
@@ -2,46 +2,52 @@ import { ImageFrame } from "../../tasks";
 import { FrameQueue } from "../queue";
 
 export abstract class ImageStream extends FrameQueue {
-    private chunks: Buffer[] = [];
-    private totalLength = 0;
-    private scanOffset = 0;
+    private buffer: Buffer = Buffer.alloc(0);
+    private offset = 0;
 
     protected abstract get soi(): Buffer;
     protected abstract get eoi(): Buffer;
-
     protected abstract onimage(buffer: Buffer): ImageFrame;
 
     public override clear(): void {
         super.clear();
-
-        this.chunks = [];
-        this.scanOffset = 0;
-        this.totalLength = 0;
+        this.buffer = Buffer.alloc(0);
+        this.offset = 0;
     }
 
     public ondata(chunk: Buffer): void {
-        this.chunks.push(chunk);
-        this.totalLength += chunk.length;
+        const soi = this.soi;
+        const eoi = this.eoi;
 
-        let buffer = Buffer.concat(this.chunks, this.totalLength);
-        let start = buffer.indexOf(this.soi, this.scanOffset);
-        let end: number;
-
-        while (
-            start !== -1 &&
-            (end = buffer.indexOf(this.eoi, start + 2)) !== -1
-        ) {
-            const image = buffer.subarray(start, end + 2);
-            this.push(this.onimage(image));
-            this.scanOffset = end + 2;
-            start = buffer.indexOf(this.soi, this.scanOffset);
+        if (this.offset === this.buffer.length) {
+            this.buffer = chunk;
+            this.offset = 0;
+        } else {
+            this.buffer = Buffer.concat(
+                [this.buffer.subarray(this.offset), chunk],
+                this.buffer.length - this.offset + chunk.length,
+            );
+            this.offset = 0;
         }
 
-        if (this.scanOffset > 0) {
-            buffer = buffer.subarray(this.scanOffset);
-            this.chunks = [buffer];
-            this.totalLength = buffer.length;
-            this.scanOffset = 0;
+        while (true) {
+            const start = this.buffer.indexOf(soi, this.offset);
+            if (start === -1) break;
+
+            const end = this.buffer.indexOf(eoi, start + soi.length);
+            if (end === -1) break;
+
+            const frame = this.buffer.subarray(start, end + eoi.length);
+            this.push(this.onimage(frame));
+
+            this.offset = end + eoi.length;
+        }
+
+        if (this.offset > 0) {
+            if (this.offset === this.buffer.length) {
+                this.buffer = Buffer.alloc(0);
+                this.offset = 0;
+            }
         }
     }
 }
