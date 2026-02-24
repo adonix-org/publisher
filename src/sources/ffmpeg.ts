d8026aa741536806be107b35bb2c56286ca4da02
Ty Busby <tybusby@gmail.com>
2026-02-24 09:53:03 -0500

    refactor

--- src/sources/ffmpeg.ts
+++ src/sources/ffmpeg.ts
@@ -2,76 +2,30 @@ import { ChildProcessWithoutNullStreams, spawn } from "node:child_process";
 import { ImageSource } from ".";
 import { Lifecycle } from "../lifecycle";
 import { ImageFrame } from "../tasks";
-import { FrameQueue } from "./queue";
+import { ImageStream } from "./stream";
 
 export class Ffmpeg extends Lifecycle implements ImageSource {
-    private static readonly JPEG_START = Buffer.from([0xff, 0xd8]);
-    private static readonly JPEG_END = Buffer.from([0xff, 0xd9]);
-
     private process: ChildProcessWithoutNullStreams | null = null;
 
     constructor(
         private readonly args: string[],
-        private readonly frames: FrameQueue = new FrameQueue(),
+        private readonly stream: ImageStream,
     ) {
-        super(frames);
+        super(stream);
     }
 
     public async next(): Promise<ImageFrame | null> {
-        return this.frames.next();
-    }
-
-    protected onimage(buffer: Buffer): ImageFrame {
-        return {
-            image: { buffer, contentType: "image/jpeg" },
-            seek: Date.now() / 1000,
-            version: 1,
-            annotations: [],
-        };
-    }
-
-    private chunks: Buffer[] = [];
-    private totalLength = 0;
-    private scanOffset = 0;
-
-    protected ondata(chunk: Buffer): void {
-        this.chunks.push(chunk);
-        this.totalLength += chunk.length;
-
-        let buffer = Buffer.concat(this.chunks, this.totalLength);
-        let start = buffer.indexOf(Ffmpeg.JPEG_START, this.scanOffset);
-        let end: number;
-
-        while (
-            start !== -1 &&
-            (end = buffer.indexOf(Ffmpeg.JPEG_END, start + 2)) !== -1
-        ) {
-            const image = buffer.subarray(start, end + 2);
-            this.frames.push(this.onimage(image));
-            this.scanOffset = end + 2;
-            start = buffer.indexOf(Ffmpeg.JPEG_START, this.scanOffset);
-        }
-
-        if (this.scanOffset > 0) {
-            buffer = buffer.subarray(this.scanOffset);
-            this.chunks = [buffer];
-            this.totalLength = buffer.length;
-            this.scanOffset = 0;
-        }
+        return this.stream.next();
     }
 
     public override async onstart(): Promise<void> {
         await super.onstart();
 
-        this.chunks = [];
-        this.scanOffset = 0;
-        this.totalLength = 0;
-
-        this.frames.clear();
+        this.stream.clear();
 
         this.process = spawn("/opt/homebrew/bin/ffmpeg", this.args);
         this.process.stdout.on("data", (chunk) => {
-            this.ondata(chunk);
+            this.stream.ondata(chunk);
         });
         this.process.stderr.on("data", (chunk) => {
             console.error(chunk.toString());
--- src/sources/rtsp.ts
+++ src/sources/rtsp.ts
@@ -1,10 +1,23 @@
+import { ImageFrame } from "../tasks";
 import { Ffmpeg } from "./ffmpeg";
+import { JpegStream } from "./stream";
+
+class RtspStream extends JpegStream {
+    protected onimage(buffer: Buffer): ImageFrame {
+        return {
+            image: { buffer, contentType: "image/jpeg" },
+            seek: Date.now() / 1000,
+            version: 1,
+            annotations: [],
+        };
+    }
+}
 
 export class Rtsp extends Ffmpeg {
     private static readonly MAX_FPS = 15;
 
     constructor(url: string, interval: number = 5) {
-        const fps = Math.min(1 / interval, Rtsp.MAX_FPS);
+        const fps = Math.min(Rtsp.MAX_FPS, 1 / interval);
         const args = [
             "-loglevel",
             "fatal",
@@ -23,7 +36,7 @@ export class Rtsp extends Ffmpeg {
             "pipe:1",
         ];
 
-        super(args);
+        super(args, new RtspStream());
     }
 
     public override toString(): string {
