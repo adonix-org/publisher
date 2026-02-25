81125397f3acff8edb82df6be99788b7c1d05f77
Ty Busby <tybusby@gmail.com>
2026-02-25 14:50:57 -0500

    disable agent profiler

--- src/agents/watchdog.ts
+++ src/agents/watchdog.ts
@@ -1,21 +1,23 @@
+import path from "node:path";
+
 import { LogError } from "../tasks/error/log";
 import { C121 } from "../sources/c121";
 import { Watermark } from "../tasks/transform/watermark";
-import { ProfileAgent } from "./profile";
 import { Remote } from "../tasks/remote/remote";
 import { ConfidenceFilter } from "../tasks/filter/confidence";
 import { SubjectFilter } from "../tasks/filter/subject";
 import { ActivityFilter } from "../tasks/filter/activity";
 import { Save } from "../tasks/transfer/save";
-import path from "node:path";
+import { Agent } from "./agent";
+import { Profiler } from "../tasks/observe/profiler";
 
-export class Watchdog extends ProfileAgent {
+export class Watchdog extends Agent {
     constructor() {
         const folder = process.env.LOCAL_IMAGE_FOLDER!;
         const camera = new C121(5);
         super(camera);
 
-        this.addImageTask(new Remote("mega"));
+        this.addImageTask(new Profiler(new Remote("mega")));
         this.addImageTask(new ConfidenceFilter(0.3));
         this.addImageTask(new SubjectFilter("person", 0.4));
         this.addImageTask(new SubjectFilter("animal", 0.5));
