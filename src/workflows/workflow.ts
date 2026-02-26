5648cd3cbeb4d99e16f8a14b2db85dd48dfc3462
Ty Busby <tybusby@gmail.com>
2026-02-26 10:33:57 -0500

    abstract workflow, do not queue image if not running

--- src/workflows/workflow.ts
+++ src/workflows/workflow.ts
@@ -2,13 +2,15 @@ import { ImageFrame, ImageTask } from "../tasks";
 import { Agent } from "../agents/agent";
 import { ImageQueue } from "../sources/queue";
 
-export class Workflow extends Agent implements ImageTask {
+export abstract class Workflow extends Agent implements ImageTask {
     constructor(private readonly queue: ImageQueue = new ImageQueue()) {
         super(queue);
     }
 
     public async process(frame: ImageFrame): Promise<ImageFrame | null> {
-        this.push(frame);
+        if (this.running) {
+            this.push(frame);
+        }
 
         return frame;
     }
