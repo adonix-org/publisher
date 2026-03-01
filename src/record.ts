47951b18b8f6d3f027b7f4d058b63cc05af83e5a
Ty Busby <tybusby@gmail.com>
2026-03-01 16:22:02 -0500

    added mpv support

--- src/record.ts
+++ src/record.ts
@@ -5,13 +5,13 @@ import { application } from "./application";
 import { Rtsp } from "./sources/rtsp";
 
 import { Recorder } from "./targets/recorder";
-import { Preview } from "./targets/preview";
+import { MPV } from "./targets/mpv";
 
 const C121_RTSP_URL = process.env.C121_RTSP_URL!;
 
 const rtsp = new Rtsp(C121_RTSP_URL);
 
-const preview = new Preview(rtsp, "mpegts", "LiveMotion");
+const preview = new MPV(rtsp);
 const recorder = new Recorder(rtsp, "/Users/tybusby/Camera/recordings");
 
 application.register(rtsp, preview, recorder);
---
+++ src/targets/mpv.ts
@@ -0,0 +1,54 @@
+import { ImageFrame, ImageTask } from "../tasks";
+import { StreamProvider } from "../sources/rtsp";
+import { BufferedPipe } from "./pipe";
+import { Executable } from "../spawn/executable";
+
+export class MPV extends Executable implements ImageTask {
+    private pipe: BufferedPipe | undefined;
+
+    constructor(private readonly provider: StreamProvider) {
+        super();
+    }
+
+    protected override executable(): string {
+        return "/opt/homebrew/bin/mpv";
+    }
+
+    protected override args(): string[] {
+        const args = [
+            "-",
+            "--force-window=yes",
+            "--input-terminal=no",
+            "--no-resume-playback",
+            "--idle=no",
+        ];
+        return args;
+    }
+
+    protected override async onstart(): Promise<void> {
+        await super.onstart();
+
+        this.pipe = new BufferedPipe(
+            this.provider.getStream(),
+            this.child.stdin,
+        );
+
+        await this.pipe.start();
+    }
+
+    protected override async onstop(): Promise<void> {
+        await this.pipe?.end();
+
+        await super.onstop();
+    }
+
+    public async process(frame: ImageFrame): Promise<ImageFrame | null> {
+        if (!this.running) return frame;
+
+        return frame;
+    }
+
+    public override toString(): string {
+        return `${super.toString()}[MPV]`;
+    }
+}
