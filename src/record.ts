fde209ef675e2391073f8359800df990ffed935d
Ty Busby <tybusby@gmail.com>
2026-02-28 21:16:40 -0500

    checkpoint

--- src/record.ts
+++ src/record.ts
@@ -5,17 +5,14 @@ import { application } from "./application";
 import { Rtsp } from "./sources/rtsp";
 import { TransportStream } from "./sources/streams/transport";
 import { RecordTransportStream } from "./targets/record";
-import { Preview } from "./targets/preview";
 
 const C121_RTSP_URL = process.env.C121_RTSP_URL!;
 
 const recorder = new RecordTransportStream("/Users/tybusby/Camera/recordings");
-const preview = new Preview("mpegts", "LiveMotion");
 
 const stream = new TransportStream();
 stream.addConsumer(recorder);
-stream.addConsumer(preview);
 
 const rtsp = new Rtsp(stream, C121_RTSP_URL);
-application.register(preview, rtsp);
+application.register(rtsp, recorder);
 application.start();
